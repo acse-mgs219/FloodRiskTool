71ae8fa29af2fb20bed6d529286abd71d7948f2b
ll2819 <ll2819@ic.ac.uk>
2019-10-23 16:57:18 +0100

    Edit function for the rainfall reading at specific date and sort the dataframe by station&datetime

--- flood_tool/api.py
+++ flood_tool/api.py
@@ -1,12 +1,53 @@
 import json
+import requests
+import csv
+import matplotlib as plt
+import pandas as pd
 import urllib.request
+import shutil
 
-#locu_api=   #key
-url='http://environment.data.gov.uk/flood-monitoring/data/readings?parameter=rainfall&_limit=50'        #url link 
-json_obj=urllib.request.urlopen(url)
+"""
+this uses Environment Agency rainfall data from the real-time data API (Beta)
+"""
 
-data=json.load(json_obj)
+#url='http://environment.data.gov.uk/flood-monitoring/data/readings?parameter=rainfall&_limit=50'        #url link 
 
-for item in data['items']:  
-    print (item['@id'])
-    print (item['value'])
+def rainfall_date(date):
+    urlbase = "http://environment.data.gov.uk/flood-monitoring/data/readings.csv?date="
+    url=urlbase+date
+    r = requests.get(url, verify=False,stream=True)
+    
+    if r.status_code != 200:
+        print("Failure!!")
+        exit()
+    else:
+        r.raw.decode_content = True
+        with open("file1.csv", 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        print("Success")
+    
+    df=pd.read_csv("file1.csv")
+    nb_rows=len(df.index)
+
+   # dfstation=df.groupby(['measure','dateTime'])
+   # print(dfstation.head())
+    df['station']='default'
+    for i in range(nb_rows):
+        urlregion=df.measure[i]
+        df.station[i]=requests.get(urlregion).json()['items']['stationReference']
+   # print(df.head())
+
+    dfstation=df.groupby(['station','dateTime'])
+    station_ref_list=dfstation['station']
+    print(station_ref_list)
+    #print(dfstation.region.unique())
+
+rainfall_date("2018-09-29") 
+
+
+
+
+
+
+
+
