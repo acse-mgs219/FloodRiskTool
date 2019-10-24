63d7fddbebea52dedc4bf672868564311b2fa658
acse-CL219 <chao-lun.liu19@imperial.ac.uk>
2019-10-24 17:14:24 +0100

    score 4544 points

--- flood_tool/tool.py
+++ flood_tool/tool.py
@@ -154,7 +154,7 @@ class Tool(object):
             data column is named `Probability Band`. Invalid postcodes and duplicates
             are removed.
         """
-	postcodes = [postcode.replace(' ', '').upper().strip() for postcode in postcodes]
+        postcodes = [postcode.replace(' ', '').upper().strip() for postcode in postcodes]
         latLongs = self.get_lat_long(postcodes)
         eastNorths = get_easting_northing_from_lat_long(latLongs[:, 0], latLongs[:, 1])
         probs = self.get_easting_northing_flood_probability(eastNorths[0], eastNorths[1])
@@ -240,7 +240,7 @@ class Tool(object):
             `Postcode` and the data column `Flood Risk`.
             Invalid postcodes and duplicates are removed.
         """
-	postcodes = [postcode.replace(' ', '').upper().strip() for postcode in postcodes]
+        postcodes = [postcode.replace(' ', '').upper().strip() for postcode in postcodes]
         latLongs = self.get_lat_long(postcodes)
         eastNorths = get_easting_northing_from_lat_long(latLongs[:, 0], latLongs[:, 1])
         probs = self.get_easting_northing_flood_probability(eastNorths[0], eastNorths[1])
